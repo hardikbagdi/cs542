927571622761a3f2e4c485808c145c210a6e53e8
Hardik Bagdi <hbagdi1@binghamton.edu>
2016-09-24 18:09:49 -0400

    Logger class filled in

--- assignment2/src/registrationScheduler/driver/Driver.java
+++ assignment2/src/registrationScheduler/driver/Driver.java
@@ -4,8 +4,7 @@ package registrationScheduler.driver;
 public class Driver{
 
 	public static void main(String args[]) {
-		int i =-9;
-		System.out.println(i);
+		
 		System.out.println("\n The args[0] is: " + args[0]);
 
 
--- assignment2/src/registrationScheduler/util/Logger.java
+++ assignment2/src/registrationScheduler/util/Logger.java
@@ -1,33 +1,64 @@
-
 package registrationScheduler.util;
 
+
+/**
+ * @author Hardik Bagdi (hbagdi1@binghamton.edu)
+ *
+ */
 public class Logger{
-
-
-    public static enum DebugLevel { CONSTRUCTOR 
-                                   };
-
+    public static enum DebugLevel { NO_OUTPUT, DS_STORE, RESULTS_ENTRY, THREAD, CONSTRUCTOR };
     private static DebugLevel debugLevel;
-
-
+    
+    /**
+     * Sets the logger level based on an int
+     * @return none
+    */
     public static void setDebugValue (int levelIn) {
-	switch (levelIn) {
-	  case 4: debugLevel = DebugLevel.CONSTRUCTOR; break;
-	}
+		//basic validation
+    	if (levelIn < 0 || levelIn > 4) {
+			throw new IllegalArgumentException("Logger debug level should not be between 0-5.");
+		}
+    	switch (levelIn) {
+			case 0:
+				debugLevel = DebugLevel.NO_OUTPUT;
+				break;
+			case 1:
+				debugLevel = DebugLevel.DS_STORE;
+				break;
+			case 2:
+				debugLevel = DebugLevel.RESULTS_ENTRY;
+				break;
+			case 3:
+				debugLevel = DebugLevel.THREAD;
+				break;
+			case 4:
+				debugLevel = DebugLevel.CONSTRUCTOR;
+				break;
+		}
+    	return;
     }
-
+    /**
+     * Sets up the logger level- based on DebugLevel
+     * @return none
+    */
     public static void setDebugValue (DebugLevel levelIn) {
-	debugLevel = levelIn;
+    	debugLevel = levelIn;
+    	return;
     }
 
-    // @return None
-    public static void writeMessage (String     message  ,
-                                     DebugLevel levelIn ) {
-	if (levelIn == debugLevel)
-	    System.out.println(message);
+    /**
+     * Writes a message if the log level matches
+     * @return none
+    */
+    public static void writeMessage (String message, DebugLevel levelIn) {
+		if (levelIn == debugLevel)
+		    System.out.println(message);
     }
-
+    
+    /* 
+     * @return none
+     */
     public String toString() {
-	return "Debug Level is " + debugLevel;
+    	return "Debug Level is " + debugLevel;
     }
 }
