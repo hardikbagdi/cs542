fe71ac8bb3e1c804946edbfab7cb65968f9d44a4
Hardik Bagdi <hbagdi1@binghamton.edu>
2016-11-10 00:23:17 -0500

    Input validations; setup ant; correct directory structure

---
+++ assignment4/wordCount/src/wordCount/driver/Driver.java
@@ -0,0 +1,43 @@
+package wordCount.driver;
+
+import java.io.File;
+
+import wordCount.util.Logger;
+import wordCount.util.Logger.DebugLevel;
+
+public class Driver {
+	private static String usageString = "Usage: java Driver <input-file> <output-file> <iteration-count>";
+
+	public static void main(String[] args) {
+		Logger.setDebugValue(DebugLevel.NO_OUTPUT);
+		if (!validateArgs(args)) {
+			System.out.println(usageString);
+			return;
+		}
+	}
+
+	private static boolean validateArgs(String[] args) {
+		try {
+			if (args.length != 3) {
+				return false;
+			}
+			if (!(new File(args[0])).canRead()) {
+				System.out.println("Input file is unreadable/doesn't exist.");
+				return false;
+			}
+			File file = new File(args[1]);
+			if (file.exists()) {
+				file.delete();
+			}
+			int iterations = Integer.parseInt(args[2]);
+			return true;
+		} catch (NumberFormatException exception) {
+			System.out.println("Please provide a natural number for iteration count.");
+			return false;
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		return false;
+	}
+}
